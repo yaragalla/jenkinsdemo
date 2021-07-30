4a1fc24ee164244d0a09895bdb0dbd0c5a3e9111
yaragalla <murali.yaragalla@gmail.com>
2021-07-30 11:04:27 +0530

    Update JenkinsDemo.java
    addded for jenkins demo test

--- JenkinsDemo.java
+++ JenkinsDemo.java
@@ -1,5 +1,5 @@
 class JenkinsDemo{
 public static void main(String args[]){
-  System.out.println("My first jenkins");
+  System.out.println("Hurray!! you are done if your output is Finished: SUCCESS");
   }
 }
